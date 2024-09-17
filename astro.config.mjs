ea1444afb8e183181e81de3cdc3d3539dc9e705b
David Gagne <buildpax@gmail.com>
2024-09-17 16:18:11 -0400

    Set site value in Astro config to branch URL for og-image testing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), icon()]
+  site: "https://spec-design-monolith--happyroll.netlify.app/", // TODO: Set to domain when live.
+  integrations: [mdx(), sitemap(), icon()],
 });
