e30963f0d229ddcfa2e25b81e792353983a7a6f7
David Gagne <buildpax@gmail.com>
2024-09-21 07:59:50 -0400

    Added favicon.ico, og-image, and removed video element.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://happyroll.netlify.app", // TODO: Set to domain when live.
   integrations: [mdx(), sitemap()],
 });
